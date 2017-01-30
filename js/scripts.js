8560012fa023b03b18a5a85922f1b1532ad7cbaa
Azure Rose Legacy and Koji Nakagawa <koji@>
2017-01-30 10:19:55 -0800

    add player movement

--- js/scripts.js
+++ js/scripts.js
@@ -8,15 +8,42 @@ $(function(){
   document.addEventListener("keydown", keyDownHandler, false);
   document.addEventListener("keyup", keyUpHandler, false);
 
-  function keyDownHandler(e) {
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  var x = canvas.width/2;
+  var y = canvas.height-30;
+  var ballRadius = 10;
+  var dx = 2;
+  var dy = -2;
+  var userHeight = 30;
+  var userWidth = 30;
+
+  var player = {
+    userX: (canvas.width - userWidth)/2,
+    userY: (canvas.height - userHeight)/2
+  };
+
+
+  function drawPlayer(){
+    ctx.beginPath();
+    ctx.arc(player.userX, player.userY, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+
+  }
+
+
+  //CONTROLLER
+    function keyDownHandler(e) {
     if(e.keyCode === 39) {
       rightPressed = true;
       console.log(rightPressed)
-    } else if (e.keyCode === 38) {
+    } else if (e.keyCode === 40) {
       downPressed = true;
     } else if(e.keyCode === 37) {
       leftPressed = true;
-    } else if(e.keyCode === 40) {
+    } else if(e.keyCode === 38) {
       upPressed = true;
     }
   }
@@ -25,23 +52,16 @@ $(function(){
     if(e.keyCode === 39) {
       rightPressed = false;
       console.log(rightPressed)
-    } else if (e.keyCode === 38) {
+    } else if (e.keyCode === 40) {
       downPressed = false;
     } else if(e.keyCode === 37) {
       leftPressed = false;
-    } else if(e.keyCode === 40) {
+    } else if(e.keyCode === 38) {
       upPressed = false;
     }
   }
 
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
-  var x = canvas.width/2;
-  var y = canvas.height-30;
-  var dx = 2;
-  var dy = -2;
 
-  var ballRadius = 10;
 
 
   function randomNumber(min,max) {
@@ -71,6 +91,7 @@ $(function(){
 
   function draw() {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
+      drawPlayer();
       drawBall();
       drawWall(200,200,100,100);
       drawWall(500,400,50,50);
@@ -85,7 +106,15 @@ $(function(){
           dy = -dy;
       }
 
-
+      if(rightPressed) {
+        player.userX += 5;
+      } else if(leftPressed) {
+        player.userX -= 5;
+      } else if(upPressed) {
+        player.userY -= 5;
+      } else if(downPressed) {
+        player.userY += 5;
+      }
   }
 
   setInterval(draw, 10);
